f32c030c3e468f10c50639bcfee4e8ea73df3568
hrookim <hrookim@gmail.com>
2022-03-20 03:15:06 +0900

    0320 2206

---
+++ 0320/2206 벽부수고 이동하기/2206_3.py
@@ -0,0 +1,36 @@
+# 이동할 때, 벽을 하나는 부수고 이동해도 괜찮다면 그 최단경로는 어떻게 구할까
+# (1,1) -> (N,M)
+import sys
+from collections import deque
+sys.stdin = open('input2.txt')
+
+
+def bfs(i, j):
+    to_visit = deque([[i, j, 0]])
+    visited[i][j][0] = 1
+    while to_visit:
+        ci, cj, crushed = to_visit.popleft()
+        
+        if (ci, cj) == (N-1, M-1):
+            return visited[ci][cj][crushed]
+        
+        for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < N and 0 <= nj < M:
+                # 벽 부수고 이동하기 (무조건 처음 만난 벽 하나를 부수게 되어있다.)
+                if matrix[ni][nj] and not crushed:
+                    to_visit.append([ni, nj, crushed+1])
+                    visited[ni][nj][crushed+1] = visited[ci][cj][crushed] + 1
+                # 벽 안 부수고 이동하기 (그냥 원래 하던대로)
+                if matrix[ni][nj]==0 and visited[ni][nj][crushed]==0:
+                    to_visit.append([ni, nj, crushed])
+                    visited[ni][nj][crushed] = visited[ci][cj][crushed] + 1
+    else:
+        return -1
+
+
+N, M = map(int, input().split())
+matrix = [list(map(int, list(input()))) for _ in range(N)]
+# 벽을 안 부쉈다면 왼쪽 좌표에 저장, 부쉈다면 오른쪽 좌표에 저장
+visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+print(bfs(0, 0))
