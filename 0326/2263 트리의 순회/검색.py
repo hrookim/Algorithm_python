5b7e48a59e75884bcbe07c67b4adb9084dda2d2b
hrookim <hrookim@gmail.com>
2022-04-13 23:43:12 +0900

    0326 2263 add

---
+++ 0326/2263 트리의 순회/검색.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('input2.txt')
+
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+inorder = list(map(int,input().split()))
+postorder = list(map(int,input().split()))
+position = [0]*(n+1)
+for i in range(n):
+    position[inorder[i]] = i
+
+
+def preorder(istart,iend,pstart,pend):
+    if istart > iend or pstart > pend:
+        return
+
+    root = postorder[pend]
+    print(root,end=' ')
+
+    left_cnt = position[root] - istart
+    right_cnt = iend - position[root]
+
+    preorder(istart,position[root]-1,pstart,pstart+left_cnt-1)
+    preorder(position[root]+1,iend,pend-right_cnt,pend-1)
+
+
+preorder(0,n-1,0,n-1)
