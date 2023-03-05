94bcc81a85e53579d7e26157ba81dfec6184a3de
hrookim <hrookim@gmail.com>
2023-03-05 16:27:28 +0900

    [settings] move directory

--- 0313/16236 아기 상어/16236.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.stdin = open('input.txt')
-
-T = int(input())
-for _ in range(T):
-    N = int(input())
--- 0514/[BOJ_11728]배열 합치기/BOJ_11728.py
+++
@@ -1,9 +0,0 @@
-import sys 
-sys.stdin = open('input3.txt') 
-input = sys.stdin.readline
-
-input()
-az = list(map(int, input().split()))
-bz = list(map(int, input().split()))
-totalz = sorted(az + bz)
-print(*totalz)
