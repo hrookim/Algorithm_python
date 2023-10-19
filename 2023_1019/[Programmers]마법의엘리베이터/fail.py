862dc3a807b22488b580029a00418807147b7615
hrookim <hrookim@gmail.com>
2023-10-19 19:57:45 +0900

    [fail] Programmers_마법의엘리베이터

---
+++ 2023_1019/[Programmers]마법의엘리베이터/fail.py
@@ -0,0 +1,27 @@
+def solution(storey):
+    answer = 0
+
+    Q = storey
+
+    while Q >= 10:
+        R = Q % 10
+        Q = Q // 10
+
+        if R < 5:
+            answer += R
+        elif R > 5:
+            answer += 10 - R
+            Q += 1
+        elif R == 5:
+            answer += 5
+            if Q % 10 >= 5:
+                Q += 1
+        
+    else:
+        answer += Q
+
+    return answer
+
+
+print(solution(2554))
+print(solution(678))
