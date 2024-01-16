5be9888d884227a1937a3aedf29ad641ee82ae45
hrookim <hrookim@gmail.com>
2024-01-16 23:40:33 +0900

    [solve] BOJ_10773

--- 2024_0116/[BOJ_10773]제로/BOJ_10773.py
+++ 2024_0116/[BOJ_10773]제로/BOJ_10773.py
@@ -1,2 +1,17 @@
 import sys 
-sys.stdin = open('input1.txt') 
+sys.stdin = open('input2.txt') 
+
+K = int(input())
+
+stack = list()
+
+for k in range(K):
+    tmp = int(input())
+    
+    if tmp == 0 and len(stack) > 0:
+        stack.pop()
+    else:
+        stack.append(tmp)
+
+print(sum(stack))
+    
